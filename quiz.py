8747cb017436042473ae3b354bc6da1e637a97f8
Richard Williams <richard.williams@thedatalab.org>
2024-12-10 09:55:24 +0000

    Added hints to each question

--- quiz.py
+++ quiz.py
@@ -2,7 +2,7 @@
 
 from quiz_answers import questions
 
-from ehrql import codelist_from_csv, debug, months
+from ehrql import codelist_from_csv, show, months
 from ehrql.tables.core import clinical_events
 
 
@@ -35,22 +35,27 @@ questions[0].check(
 # Question 1
 # Create a patient series containing the date of each patient's earliest diabetes diagnosis.
 questions[1].check(...)
+# If you need a hint for this, or any other, question, just uncomment (remove the #) from the following line:
+# questions[1].hint()
 
 # Question 2
 # Create a patient series containing the date of each patient's earliest structured education
 # programme referral. (Use the referral_code codelist.)
 questions[2].check(...)
+# questions[2].hint()
 
 # Question 3
 # Create a boolean patient series indicating whether the date of each patient's earliest diabetes
 # diagnosis was between 1st April 2023 and 31st March 2024. If the patient does not have a
 # diagnosis, the value for in this series should be False.
 questions[3].check(...)
+# questions[3].hint()
 
 # Question 4
 # Create a patient series indicating the number of months between a patient's earliest diagnosis
 # and their earliest referral.
 questions[4].check(...)
+# questions[4].hint()
 
 # Question 5
 # Create a boolean patient series identifying patients who have been diagnosed with diabetes for
@@ -58,29 +63,38 @@ questions[4].check(...)
 # being referred to a structured education programme within nine months after their diagnosis.
 
 questions[5].check(...)
+# questions[5].hint()
 
 # Question 6
 # Create a patient series with the date of the latest record of mild frailty for each patient.
 questions[6].check(...)
+# questions[6].hint()
 
 # Question 7
 # Create a patient series with the date of the latest record of moderate or severe frailty for
 # each patient.
 questions[7].check(...)
+# questions[7].hint()
 
 # Question 8
-# Create a boolean patient series indicating whether a patient's last record of severity is
-# moderate or severe. If the patient does not have a record of frailty, the value in this series
-# should be False.
+# A patient may have mild, moderate and severe frailty codes in their record. A patient's frailty
+# is considered to be their most recent frailty code. So if their most recent frailty code was for
+# mild frailty, then we would say they have mild frailty.
+# Create a boolean patient series indicating whether a patient has moderate or severe frailty, i.e
+# where the patient's last record of severity is moderate or severe. If the patient does not have
+# a record of frailty, the value in this series should be False.
 questions[8].check(...)
+# questions[8].hint()
 
 # Question 9
 # Create a patient series containing the latest HbA1c measurement for each patient.
 questions[9].check(...)
+# questions[9].hint()
 
 # Question 10
 # Create a boolean patient series identifying patients without moderate or severe frailty in whom
 # the last IFCC-HbA1c is 58 mmol/mol or less
 questions[10].check(...)
+# questions[10.hint()
 
 questions.summarise()
